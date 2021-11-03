12f0d518c0f8c09f98de5b9bc6dad3f82325ff9a
minachraf <minachraf@outlook.com>
2021-11-04 01:30:31 +0200

    BFS API return done

--- BFS.py
+++ BFS.py
@@ -1,33 +1,23 @@
 from helpers import *
 from queue import Queue
 
+
 def bfs(current):
     current = str_to_array(current)
     final = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
-    original = current
     row, col = search_zero(current)
     _queue = Queue()
     _queue.put((current, row, col))
     _queue.put((-1, -1, -1))
     visited = set()
     visited.add(array_to_string(current))
-    counter = 0
-    history = []
     phase = 0
     path_history = dict()
+    path_history[array_to_string(current)] = None
     while not _queue.empty():
-        counter += 1
         current, row, col = _queue.get()
-        if current != -1:
-            history.append(current)
-        else:
-            phase += 1
-            history = []
-            counter -= 1
-            continue
         if current == final:
-            phase += 1
-            return minimized_path(path_history, original)
+            return path_history, phase, len(path_history.keys())
         new_one = False
         for neighbor_row, neighbor_col in get_neighbors(row, col):
             current[row][col], current[neighbor_row][neighbor_col] = current[neighbor_row][neighbor_col], current[row][
@@ -36,6 +26,7 @@ def bfs(current):
             if new_array not in visited:
                 new_one = True
                 visited.add(new_array)
+                phase += 1
                 _queue.put((copy.deepcopy(current), neighbor_row, neighbor_col))
             current[row][col], current[neighbor_row][neighbor_col] = current[neighbor_row][neighbor_col], current[row][
                 col]
@@ -44,4 +35,4 @@ def bfs(current):
                 new_one = False
         _queue.put((-1, -1, -1))
     else:
-        return
+        return None, phase, -1
